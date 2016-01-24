be680c1ed40ee0d32224bcacfb8a13f278dbed37
Yas Okada <yas.okadatech@gmail.com>
2016-01-24 22:47:20 +0900

    POI

--- SampleMap/assets/data.js
+++ SampleMap/assets/data.js
@@ -15,12 +15,36 @@
         "name": "札幌市時計台",
         "lat": 43.062562,
         "lon": 141.3514613,
-        "image": "https://ja.wikipedia.org/wiki/%E3%83%95%E3%82%A1%E3%82%A4%E3%83%AB:Sapporo_Clock_Tower_Hokkaido_Japan.jpg"
+        "image": "https://upload.wikimedia.org/wikipedia/commons/6/6b/Sapporo_Clock_Tower_Hokkaido_Japan.jpg"
     },
     {
         "name": "黒部ダム",
         "lat": 36.5663788,
         "lon": 137.6599953,
-        "image": "https://ja.wikipedia.org/wiki/%E3%83%95%E3%82%A1%E3%82%A4%E3%83%AB:Kurobe_Dam_survey_2.jpg"
+        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Kurobe_Dam_survey_2.jpg/900px-Kurobe_Dam_survey_2.jpg"
+    },
+    {
+        "name": "出雲大社",
+        "lat": 35.402035,
+        "lon": 132.6832673,
+        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/Izumo_Taisya_Haiden.jpg/240px-Izumo_Taisya_Haiden.jpg"
+    },
+    {
+        "name": "五稜郭",
+        "lat": 41.7971839,
+        "lon": 140.754642,
+        "image": "https://upload.wikimedia.org/wikipedia/ja/thumb/d/d5/Hakodate_Goryokaku_Panorama_1.JPG/550px-Hakodate_Goryokaku_Panorama_1.JPG"
+    },
+    {
+        "name": "高千穂峡",
+        "lat": 32.7018045,
+        "lon": 131.2982183,
+        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/28/Takachiho-kyo%28Gorge%29_-_River_-_%E5%B7%9D.jpg/560px-Takachiho-kyo%28Gorge%29_-_River_-_%E5%B7%9D.jpg"
+    },
+    {
+        "name": "東京スカイツリー",
+        "lat": 35.7100627,
+        "lon": 139.8085117,
+        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Tokyo_Sky_Tree_2012.JPG/600px-Tokyo_Sky_Tree_2012.JPG"
     }
 ]
